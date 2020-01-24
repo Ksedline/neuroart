812cbd0d5c1649d059047c71b557568ca23e8e06
Kirill Shekhovtsov <ksedline@gmail.com>
2020-01-24 03:39:05 +0300

    Neuro Art

---
+++ js/az/src/az.syntax.js
@@ -0,0 +1,13 @@
+; (function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? (module.exports = module.exports || {}) && (module.exports.Syntax = factory(module.exports)) :
+    typeof define === 'function' && define.amd ? define('Az.Syntax', ['Az'], factory) :
+      (global.Az = global.Az || {}) && (global.Az.Syntax = factory(global.Az))
+}(this, function (Az) {
+  'use strict';
+  // TBD: Syntax analyzer
+  var Syntax = function () {
+
+  }
+
+  return Syntax;
+}));
---
+++ precache-manifest.986e042589b391c9920cd2b6f85acd59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f442c961765cd1857ae97325bb9b779b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67f1a40d1c584b27a808",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1f0e9ea3e39c7d2fafe2",
+    "url": "./static/css/main.000b6b3c.chunk.css"
+  },
+  {
+    "revision": "67f1a40d1c584b27a808",
+    "url": "./static/js/2.8f8b951b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.8f8b951b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f0e9ea3e39c7d2fafe2",
+    "url": "./static/js/main.53341727.chunk.js"
+  },
+  {
+    "revision": "39e89f8cbac8f199936f",
+    "url": "./static/js/runtime-main.83a55eda.js"
+  }
+]);
